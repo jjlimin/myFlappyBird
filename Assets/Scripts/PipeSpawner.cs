459e5a4de74a458710238e0f54766ebff78547b4
Alfredo Limin <61728192+jjlimin@users.noreply.github.com>
2026-08-22 16:17:41 +0300

    added pipe spawner

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] private float _maxTime = 1.5f;
+    [SerializeField] private float _heightRange = 0.45f;
+    [SerializeField] private GameObject _pipePrefab;
+
+    private float _timer;
+    
+    private void  Start()
+    {
+        SpawnPipe();
+    }
+
+    private void SpawnPipe()
+    {
+        Vector3 spawnPosition = transform.position + new Vector3(0, Random.Range(-_heightRange, _heightRange));
+        GameObject _pipe = Instantiate(_pipePrefab, spawnPosition, Quaternion.identity);
+        
+        Destroy(_pipe, 10f);
+    }
+
+    private void Update()
+    {
+        if (_timer > _maxTime)
+        {
+            SpawnPipe();
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
+    }
+}
