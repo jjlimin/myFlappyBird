6ad294dc8fd7300caac65ff069935522d0ca8484
Alfredo Limin <61728192+jjlimin@users.noreply.github.com>
2026-08-24 21:51:05 +0300

    fixed spawning aspect ratio bug

--- Assets/Scripts/MovePipe.cs
+++ Assets/Scripts/MovePipe.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 public class MovePipe : MonoBehaviour
 {
     [SerializeField] private float _speed = 0.65f;
+    [SerializeField] private float _destroyMargin = 1f;
+
+    private Camera _cam;
+
+    private void Start()
+    {
+        _cam = Camera.main;
+    }
 
     private void Update()
     {
         transform.position += Vector3.left * (_speed * Time.deltaTime);
+
+        float leftEdge = _cam.transform.position.x - (_cam.orthographicSize * _cam.aspect);
+        if (transform.position.x < leftEdge - _destroyMargin)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -5,8 +5,18 @@ public class PipeSpawner : MonoBehaviour
     [SerializeField] private float _spawnInterval = 1.5f;
     [SerializeField] private float _minHeightOffset = -0.4f;
     [SerializeField] private float _maxHeightOffset = 0.9f;
-    [SerializeField] private float _pipeLifetime = 4f;
+    [SerializeField] private float _spawnMargin = 1f;
     [SerializeField] private GameObject _pipePrefab;
+
+    private void Start()
+    {
+        Camera cam = Camera.main;
+        float halfWidth = cam.orthographicSize * cam.aspect;
+        transform.position = new Vector3(
+            cam.transform.position.x + halfWidth + _spawnMargin,
+            transform.position.y,
+            transform.position.z);
+    }
 
     public void StartSpawning()
     {
@@ -16,8 +26,6 @@ public class PipeSpawner : MonoBehaviour
     private void SpawnPipe()
     {
         Vector3 spawnPosition = transform.position + new Vector3(0, Random.Range(_minHeightOffset, _maxHeightOffset));
-        GameObject pipe = Instantiate(_pipePrefab, spawnPosition, Quaternion.identity);
-
-        Destroy(pipe, _pipeLifetime);
+        Instantiate(_pipePrefab, spawnPosition, Quaternion.identity);
     }
 }
